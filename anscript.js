9cea65e9248b3e2dd016345786676abf696f8d04
Ananth Narayan <ananth.naray@gmail.com>
2023-09-04 13:02:41 -0400

    adding activities

--- anscript.js
+++ anscript.js
@@ -2,9 +2,12 @@ about = document.getElementById("about");
 projects = document.getElementById("projects");
 contact = document.getElementById("contact");
 pic = document.getElementById("pic");
+mailpic = document.getElementById("mailpic")
 text = document.getElementById("text");
 act = document.getElementById("act");
-skp = document.getElementById("skp")
+acdes = document.getElementById("acdes");
+course = document.getElementById("course")
+cdes = document.getElementById("cdes")
 click = 0
 
 function pclicked(){
@@ -19,11 +22,15 @@ function pclicked(){
     about.style.fontWeight = "400";
     text.innerHTML = "";
 	act.innerHTML = "";
-	skp.innerHTML="";
+	acdes.innerHTML="";
+    cdes.innerHTML=""
+    course.innerHTML= ""
 	text.style.marginLeft = "0";
 	pic.removeAttribute('src');
-	pic.style.opacity = "0";
-    text.innerHTML= "<a href = 'https://anarayan05.github.io/namanrush/'>namanrush</a><br><br>I programmed this game using JavaScript, it is a single player casual game. It consists of a circle that you can move using the arrow keys, and other circles that you must avoid. If your circle gets hit, you lose a life, and you have 3 lives. Your circle is also given 50 bullets. These bullets are released using the WASD controls for each direction. Along with avoiding the circles you must shoot all the circles in the field to advance to the next phase. In one of the phases, the other circles shoot back. Please give it a try, and have fun! "
+    pic.style.opacity="0"
+    mailpic.removeAttribute('src')
+    mailpic.style.opacity="0"
+    text.innerHTML= "<a href = 'https://anarayan05.github.io/namanrush/'><b>namanrush</b></a><br><br>I programmed this game using JavaScript, it is a single player casual game. It consists of a circle that you can move using the arrow keys, and other circles that you must avoid. If your circle gets hit, you lose a life, and you have 3 lives. Your circle is also given 50 bullets. These bullets are released using the WASD controls for each direction. Along with avoiding the circles you must shoot all the circles in the field to advance to the next phase. In one of the phases, the other circles shoot back. Please give it a try, and have fun!<br><br><a href = 'https://interview-project.streamlit.app/'><b>Story Loom Project</b></a><br><br>This project is about sharing the stories and journeys of Indian American Immigrants. I have recorded interviews of Indian Americans in the Buffalo Area involving their journeys, challenges and succeses coming from India to America. After that , I used artificial intelligence to transcribe the interview and convert it into text, to easily share them."
     projects.removeEventListener("mouseleave",pleave)
     about.addEventListener("mouseleave",ableave)
     contact.addEventListener("mouseleave",cleave)
@@ -37,12 +44,15 @@ function conclicked(){
     projects.style.textDecoration = "none";
     about.style.textDecoration = "none";
     text.innerHTML = "";
-	pic.style.opacity="1";
-	pic.src = "maily.png";
-	pic.style.border = "none"
+	pic.style.opacity = "0"
+    pic.removeAttribute('src')
+	mailpic.src="maily.png"
+    mailpic.style.opacity="1"
 	act.innerHTML = "";
-	skp.innerHTML="";
-	text.innerHTML = "<form action=mailto:ananth.naray@gmail.com method=get enctype=text/plain>Subject:<br><input type=text name=subject><br>Email:<br><input type=email name=email><br>Message:<br><textarea name=body rows=5 cols=30></textarea><br><input type=submit value=Send></form>";
+    cdes.innerHTML=""
+    course.innerHTML= ""
+	acdes.innerHTML="";
+	text.innerHTML = "<form action=mailto:ananth.naray@gmail.com method=get enctype=text/plain>Subject:<br><input type=text name=subject><br>Email:<br><input type=email name=email><br>Message:<br><textarea name=body rows=5 cols=30></textarea><br><input type=submit value=Send></form><br>";
     contact.removeEventListener("mouseleave",cleave)
     projects.addEventListener("mouseleave",pleave)
     about.addEventListener("mouseleave",ableave)
@@ -58,11 +68,15 @@ function aclicked(){
     text.innerHTML = "";
 	text.style.marginLeft = "30px";
 	pic.style.border = "3px solid"
-	pic.style.opacity = "1";
+    mailpic.removeAttribute('src')
+    mailpic.style.opacity="0"
 	pic.src = "njimage.jpg";
-    text.innerHTML="Hello my name is <b>Ananth Narayan</b>. I am a 12th grade highschool student amd have a passion for computer science. In my free time I like to play guitar and basketball. I love to code, whether it be problem solving or creating new projects, I always have something I am working on.";
-	act.innerHTML = "<b>Notable Activities and Awards<b>"
-	skp.innerHTML= "Proficient in 4 programming languages:<br>Python, Javascript, Java, and HTML"
+    pic.style.opacity="1"
+    act.innerHTML = "<b>Notable Activities</b>"
+    course.innerHTML = "<b>Coursework</b>"
+    cdes.innerHTML = "<li>Python Data Structures, UMich 2020</li><li>Summer High School Coding Camp (Web Development with Javascript and HTML), UPenn 2021</li>"
+    text.innerHTML="Hello my name is <b>Ananth Narayan</b>. I am a 12th grade highschool student and have a passion for computer science. In my free time I like to play guitar and basketball. I love to code, whether it be problem solving or creating new projects, I always have something I am working on. Otherwise, I love to explore other topics, such as data structures and AI. ";
+    acdes.innerHTML= "<li>Created projects (games,websites,interactive interfaces) using 4 different languages:<br>Java, Python, Javascript, and HTML. See in projects tab.</li><li>Volunteered at Meals on Wheels</li><li>Worked at Asbury Pointe Retirement Community</li><li>Organized the community based Story Loom Project to share the stories of Indian Americans in the Buffalo-Niagara Area. See in projects tab.</li><li>Placed 2nd in 2023 Lockheed Martin Codequest (Owego, New York) in Team EA<br><a href='https://www.lockheedmartin.com/en-us/who-we-are/communities/codequest/code-quest-past-quests/codequest-2023.html'>See Competition Results</a></li><li>Attended Amherst School of Guitar 2017-2023</li><li>Interned at University at Buffalo, Summer 2023. Worked on project involving developing machine learning algorithm to identify people in historical documents.</li>"
     about.removeEventListener("mouseleave",ableave)
     contact.addEventListener("mouseleave",cleave)
     projects.addEventListener("mouseleave",pleave)
